bbd3713f11bc82fc4c70a73ef99ed32085f5c5b3
DiogoGaldom <131724277+DiogoGaldom@users.noreply.github.com>
2023-06-11 23:14:30 +0000

    Add files via upload
    Este programa pede a senha digitada pelo usuario, e confirma se é a mesma senha dada pelo programador

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("digite um numero");
+        input numero = nextint();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+
